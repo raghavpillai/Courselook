b40e284b61bf3b34704538ed2ed4dbda8257e94a
Jacqueline Wu <68624038+jacqw8@users.noreply.github.com>
2023-11-04 17:30:25 -0500

    home add body

--- client/src/Home.js
+++ client/src/Home.js
@@ -99,8 +99,78 @@ function App() {
       <Box textAlign="center" fontSize="xl">
         <Navbar></Navbar>
         <Grid minH="100vh" p={30}>
+        <>
+      <Container maxW={'3xl'}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}>
+            search for anything<br />
+            <Text as={'span'} color={'green.400'}>
+            across a playlist of videos
+            </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            Directly enter a playlist link below or put individual videos into the form. Then click submit!
+          </Text>
+
+        
+          <Input placeholder="Enter Playlist Link" value={playlistVal} onChange={playlistClick}/>
+
+          <Text>or</Text>
+
+            <Container maxW={'6xl'} mt={10}>
+                <SimpleGrid columns={{ base: 1 }} spacing={10}>
+                    <VideoIn num={1} in={in1} changeIn={setIn1}/>
+                    {in1.length !== 0 ? <VideoIn num={2} in={in2} changeIn={setIn2}/> : <></>}
+                    {in1.length !== 0 && in2.length !== 0 ? <VideoIn num={3} in={in3} changeIn={setIn3}/> : <></>}
+                    {(in1.length !== 0 || in2.length !== 0) && in3.length !== 0 ? <VideoIn num={4} in={in4} changeIn={setIn4}/> : <></>}
+                    {(in1.length !== 0 || in2.length !== 0 || in3.length) && in4.length !== 0 ? <VideoIn num={5} in={in5} changeIn={setIn5}/> : <></>}
+                    {(in1.length !== 0 || in2.length !== 0 || in3.length || in4.length) && in5.length !== 0 ? <VideoIn num={6} in={in6} changeIn={setIn6}/> : <></>}
+                    {(in1.length !== 0 || in2.length !== 0 || in3.length || in4.length || in5.length !== 0) && in6.length !== 0 ? <VideoIn num={7} in={in7} changeIn={setIn7}/> : <></>}
+                    {(in1.length !== 0 || in2.length !== 0 || in3.length || in4.length || in5.length !== 0 || in6.length) !== 0 && in7.length !== 0 ? <VideoIn num={8} in={in8} changeIn={setIn8}/> : <></>}
+                    {(in1.length !== 0 || in2.length !== 0 || in3.length || in4.length || in5.length !== 0 || in6.length !== 0 || in7.length !== 0) && in8.length !== 0 ? <VideoIn num={9} in={in9} changeIn={setIn9}/> : <></>}
+                    {(in1.length !== 0 || in2.length !== 0 || in3.length || in4.length || in5.length !== 0 || in6.length !== 0 || in7.length !== 0 || in8.length !== 0) && in9.length !== 0 ? <VideoIn num={10} in={in10} changeIn={setIn10}/> : <></>}
+                </SimpleGrid>
+            </Container>
 
 
+
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+            
+            <Button
+              colorScheme={'green'}
+              bg={'green.400'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'green.500',
+              }}
+              onClick={handleSubmit}
+              >
+              Submit
+            </Button>
+
+
+            <Button variant={'link'} colorScheme={'blue'} size={'sm'} onClick={handlePremadeClick}>
+              Use Pre-made Playlist
+            </Button>
+
+            {/* {isLoading ? <Loading/> : <></>} */}
+          </Stack>
+        </Stack>
+      </Container>
+    </>
 
         </Grid>
       </Box>
