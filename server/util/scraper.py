b0c9f1472e1f5563cb64879deda4a7055882cb05
jacqw8 <jacquelinewu88@gmail.com>
2023-11-04 16:27:43 -0500

    create util folder

---
+++ server/util/scraper.py
@@ -0,0 +1,16 @@
+# process_request('videos', ['https://www.youtube.com/watch?v=8719-T6J4R0'], 'What is the meaning of life?')
+# Scraper.process_request('playlist', "https://www.youtube.com/playlist?list=PLBlnK6fEyqRgp46KUv4ZY69yXmpwKOIev", 'What is a finite state?')
+
+# video_id = video_url.split('v=')[-1]
+# download_transcript(video_id)
+
+# https://www.youtube.com/watch?v=8719-T6J4R0
+# https://www.youtube.com/watch?v=uHt01D6rOLI&t=1s
+
+# ?si=PFBO_KmAcpXxuCxY
+
+# https://www.youtube.com/embed/8719-T6J4R0?si=PFBO_KmAcpXxuCxY
+# https://www.youtube.com/embed/uHt01D6rOLI?si=PFBO_KmAcpXxuCxY
+# https://www.youtube.com/embed/uHt01D6rOLI?start=30
+
+# <iframe width="560" height="315" src="https://www.youtube.com/embed/8719-T6J4R0?si=PFBO_KmAcpXxuCxY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
